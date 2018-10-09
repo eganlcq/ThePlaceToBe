a88ed06750dae5a0dd838333b88512d35d9d6cfd
eganlcq <32932521+eganlcq@users.noreply.github.com>
2018-10-09 22:10:59 +0200

    Add files via upload

--- Views/ProductPage/ProductPage.xaml.cs
+++ Views/ProductPage/ProductPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace ThePlaceToBe.Views.ProductPage
@@ -15,6 +16,18 @@ namespace ThePlaceToBe.Views.ProductPage
 		public ProductPage ()
 		{
 			InitializeComponent ();
+			double lat = 50.669204;
+			double lon = 4.613774;
+			Position pos = new Position(lat, lon);
+			Pin pin = new Pin {
+				Type = PinType.Generic,
+				Position = pos,
+				Label = "Enorme étron fragile",
+				Address = "Tu te calme"
+			};
+			map.MoveToRegion(MapSpan.FromCenterAndRadius(pos, Distance.FromKilometers(0.1)));
+			map.MapType = MapType.Street;
+			map.Pins.Add(pin);
 		}
 	}
 }
