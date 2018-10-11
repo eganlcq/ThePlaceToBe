5142e8f3128f60c69a6c233ea60f81ee677cacbc
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-10-12 00:01:57 +0200

    Add files via upload

--- DB/scriptTables.sql
+++ DB/scriptTables.sql
@@ -1,4 +1,3 @@
-
 create table TbSucces(
 idsucces int(2) not null,
 nom varchar(16) not null,
@@ -6,42 +5,46 @@ primary key (idsucces)
 );
 
 create table TbBar(
-nombar varchar(16) not null,
-longitude varchar(11) not null,
-latitude varchar(11) not null,
+nombar varchar(32) not null,
+latitude double(10,7) not null,
+longitude double(10,7) not null,
 primary key (nombar)
 );
 
 create table TbAvis(
 idavis int(5) not null,
-nombar varchar(16) not null,
-avis varchar(500) not null,
+nombar varchar(32) not null,
+avis text not null,
 primary key (idavis),
 foreign key (nombar) references TbBar(nombar)
 );
 
 create table TbUser(
-iduser int(5) not null,
+iduser int not null,
 nom varchar(32) not null,
 prenom varchar(32) not null,
-mdp varchar(32) not null,
+datenaiss date not null,
 email varchar(64) not null,
-photo blob,
+mdp varchar(64) not null,
+photo blob DEFAULT null,
+nbrecherche int DEFAULT 0,
+nbajout int DEFAULT 0,
+datelastco datetime not null DEFAULT current_timestamp,
 primary key (iduser)
 );
 
 create table TbOrnement(
 iduser int(5) not null,
-idsucces int(2) not null,
+idsucces int(3) not null,
 primary key (iduser, idsucces),
 foreign key (iduser) references TbUser(iduser),
 foreign key (idsucces) references TbSucces(idsucces)
 );
 
 create table TbBiere(
-nombiere varchar(16) not null,
-alcoolemie decimal(2,1) not null,
-saveur varchar(16) not null,
+nombiere varchar(32) not null,
+alcoolemie varchar(4) not null,
+typebiere varchar(16) not null,
 image blob not null,
 primary key (nombiere)
 );
@@ -49,8 +52,8 @@ primary key (nombiere)
 
 
 create table TbCarte(
-nombiere varchar(16) not null,
-nombar varchar(16) not null,
+nombiere varchar(32) not null,
+nombar varchar(32) not null,
 primary key (nombiere , nombar),
 foreign key (nombiere) references TbBiere(nombiere),
 foreign key (nombar) references TbBar(nombar)
@@ -59,14 +62,9 @@ foreign key (nombar) references TbBar(nombar)
 
 
 create table TbFavoris(
-nombiere varchar(16) not null,
+nombiere varchar(32) not null,
 iduser int(5) not null,
 primary key (nombiere, iduser),
 foreign key (nombiere) references TbBiere(nombiere),
 foreign key (iduser) references TbUser(iduser)
 );
-
-
-
-
-
