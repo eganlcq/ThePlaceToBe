331b4c9bbf38c18347a6638afc4eb3a2c2c0cd78
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-10-13 20:11:20 +0200

    Update scriptInsertTbuser.sql

--- DB/Insertion données/scriptInsertTbuser.sql
+++ DB/Insertion données/scriptInsertTbuser.sql
@@ -2,4 +2,4 @@ ALTER TABLE TbUser auto_increment = 1;
 
 INSERT INTO TbUser(nom, prenom, datenaiss, email, mdp, photo, nbrecherche, nbajout, datelastco) VALUES
 
-('Admin', 'istrateur', 'JEJ', '1997-08-26', 'placetobe.ephec@hotmail.com', 'jej111jej222', default, default, default, default);
+('Admin', 'istrateur', 'JEJ', '1997-08-26', 'placetobe.ephec@hotmail.com', 'jej111jej222', 'image/avatar/avatar1.png', default, default, default);
