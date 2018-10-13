f1df4fe6e948bc8d4b9455f36b4ce9247e94c061
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-10-13 19:21:42 +0200

    Update scriptInsertTbuser.sql

--- DB/Insertion données/scriptInsertTbuser.sql
+++ DB/Insertion données/scriptInsertTbuser.sql
@@ -2,4 +2,4 @@ ALTER TABLE tbuser auto_increment = 1;
 
 INSERT INTO tbuser(nom, prenom, datenaiss, email, mdp, photo, nbrecherche, nbajout, datelastco) VALUES
 
-('Admin', 'istrateur', '1997-08-26', 'placetobe.ephec@hotmail.com', 'jej111jej222', default, default, default, default);
+('Admin', 'istrateur', 'JEJ', '1997-08-26', 'placetobe.ephec@hotmail.com', 'jej111jej222', default, default, default, default);
