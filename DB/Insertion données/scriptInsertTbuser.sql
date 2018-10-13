9a2e53b8a48c96a9c77112ffb16bbd69f31b9896
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-10-13 20:51:27 +0200

    Add files via upload

--- DB/Insertion données/scriptInsertTbuser.sql
+++ DB/Insertion données/scriptInsertTbuser.sql
@@ -1,5 +1,5 @@
-ALTER TABLE tbuser auto_increment = 1;
+ALTER TABLE TbUser auto_increment = 1;
 
-INSERT INTO tbuser(nom, prenom, datenaiss, email, mdp, photo, nbrecherche, nbajout, datelastco) VALUES
+INSERT INTO TbUser(nom, prenom, pseudo, datenaiss, email, mdp, photo, nbrecherche, nbajout, datelastco) VALUES
 
-('Admin', 'istrateur', 'JEJ', '1997-08-26', 'placetobe.ephec@hotmail.com', 'jej111jej222', 'image/avatar/avatar1.png', default, default, default);
+('Admin', 'istrateur', 'JEJ', '1997-08-26', 'placetobe.ephec@hotmail.com', 'jej111jej222', 'image\\avatar\\avatar1.png', default, default, default);
