438cc846f91fbdb0864eb270c2afeac9e412d457
eganlcq <32932521+eganlcq@users.noreply.github.com>
2018-10-17 22:43:58 +0200

    Add files via upload

--- App/App.xaml.cs
+++ App/App.xaml.cs
@@ -4,24 +4,25 @@ using Android.Content.PM;
 using Android.Support.V4.App;
 using Android.Support.V4.Content;
 using System;
+using ThePlaceToBe.Views;
 using ThePlaceToBe.Views.AchievementPage;
 using ThePlaceToBe.Views.ConnexionPage;
 using ThePlaceToBe.Views.InscriptionPage;
 using ThePlaceToBe.Views.MainPage;
 using ThePlaceToBe.Views.ProductPage;
 using ThePlaceToBe.Views.ScanPage;
-using ThePlaceToBe.Views.Test;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace ThePlaceToBe {
 	public partial class App : Application {
-		
+
 
 		public App() {
 			InitializeComponent();
-			MainPage = new MainPage();
+			NavigationPage.SetHasNavigationBar(this, false);
+			MainPage = new NavigationPage(new ConnexionPage());
 		}
 
 		protected override void OnStart() {
