5c630dcb014161d5f68b7cccf4c6c0eea9c97640
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-10-18 19:14:49 +0200

    Update mainPage.inc.php

--- Site_Web/INC/mainPage.inc.php
+++ Site_Web/INC/mainPage.inc.php
@@ -4,6 +4,7 @@
     <?php if(count(get_included_files())==1) die('--access denied--'); ?>
     <title>ThePlaceToBe</title>
     <meta charset=utf-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/mainPage.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="JS/index.js"></script>
