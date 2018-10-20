73c85045f1b972bc6217d635c5414d58059bd567
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-10-20 15:43:02 +0200

    Add files via upload

---
+++ Views/Data/RestService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePlaceToBe.Data
+{
+    public class RestService
+    {
+
+        public static HttpClient client;
+        public static Dictionary<string, string> dic;
+
+        public RestService()
+        {
+        }
+
+        public static async Task<List<Beer>> Request(Dictionary<string, string> dic, string page)
+        {
+
+            if (client == null) client = new HttpClient();
+            Dictionary<string, string> values = dic;
+            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+            HttpResponseMessage response = await client.PostAsync("http://51.38.239.219/admin/" + page + ".php", content).ConfigureAwait(false);
+            string responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Beer>>(responseString);
+        }
+    }
+}
