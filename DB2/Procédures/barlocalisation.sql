c0d0c348f76bb92df744960a487e8cacc3c05ae0
eganlcq <32932521+eganlcq@users.noreply.github.com>
2018-10-20 14:34:27 +0200

    Delete barlocalisation.sql

--- DB2/Procédures/barlocalisation.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `barlocalisation`(beerid varchar(32))
-BEGIN
-SELECT tbba.idbar, tbba.nombar, tbba.latitude, tbba.longitude
-FROM TbBar AS tbba
-JOIN TbCarte AS tbc ON tbc.idbar = tbba.idbar
-JOIN tbBiere AS tbbi ON tbbi.idbiere = tbc.idbiere
-WHERE tbbi.idbiere = beerid;
-END
