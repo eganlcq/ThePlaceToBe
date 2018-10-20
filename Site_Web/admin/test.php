612e0e34ba93e880ef8c669eb9c3ddbbd935bf83
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-10-20 15:38:12 +0200

    Add files via upload

--- Site_Web/admin/test.php
+++ Site_Web/admin/test.php
@@ -1,15 +1,33 @@
 <?php
     require_once 'database.php';
 
-    $id = 0;
+    $firstName              = "";
+    $name                   = "";
+    $pseudo                 = "";
+    $email                  = "";
+    $password               = "";
 
+    if(!empty($_POST)) {
+        $firstName          = $_POST['firstName'];
+        $name               = $_POST['name'];
+        $pseudo             = $_POST['pseudo'];
+        $email              = $_POST['email'];
+        $password           = $_POST['password'];
+        
+        
+    }
+        
+   
     $db = Database::connect();
-    $sql =  'select * from TbBiere;';
-    $statement = $db->prepare($sql);
-    $statement->execute();
-    $item = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $preSql = 'alter table TbUser auto_increment = 1';
+    $preStatement = $db->prepare($preSql);
+    $preStatement->execute();
 
-    //echo '<pre>' . print_r($item, true) . '</pre>';
+    $sql =  'insert into TbUser values (2, ?, ?, ?, null, ?, ?, default, default, default, default);';
+    $statement = $db->prepare($sql);
+    $statement->execute([$firstName, $name, $pseudo, $email, $password]);
+    $item = $statement->fetchAll(PDO::FETCH_ASSOC);
     Database::disconnect();
+
     echo json_encode($item);
 ?>
