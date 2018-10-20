2b392c7403c7d2eae4811c812f47ff3ad66c9752
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-10-20 16:50:49 +0200

    Add files via upload

---
+++ Views/Data/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePlaceToBe.Views.Data
+{
+    public class User
+    {
+        public int Iduser { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public string Datenaiss { get; set; }
+        public DateTime DatenaissDateTime { get; set; }
+
+        public string Photo { get; set; }
+
+
+        public User(){
+            
+        }
+    }
+}
