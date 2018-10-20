aeadd410e77d6f842c0a51b1e7408ff872f5fb81
eganlcq <32932521+eganlcq@users.noreply.github.com>
2018-10-20 14:34:05 +0200

    Delete achievementbyuser.sql

--- DB2/Procédures/achievementbyuser.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `achievementbyuser`(userid int)
-BEGIN
-SELECT tbs.idsucces, tbs.nom, tbs.descr
-FROM TbSucces AS tbs
-JOIN TbOrnement AS tbo ON tbo.idsucces = tbs.idsucces
-JOIN TbUser AS tbu ON tbu.iduser = tbo.iduser 
-WHERE tbo.iduser = userid;
-END
