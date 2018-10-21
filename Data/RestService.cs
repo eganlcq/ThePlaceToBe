ae4eb4bbca54e624ab4967f62b729b3670cd5247
jordanvkb201460 <33064840+jordanvkb201460@users.noreply.github.com>
2018-10-21 18:00:23 +0200

    Add files via upload

--- Data/RestService.cs
+++ Data/RestService.cs
@@ -5,23 +5,49 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ThePlaceToBe.Data {
-	public class RestService {
+namespace ThePlaceToBe
+{
+    class RestService
+    {
+        public static HttpClient client;
+        public static Dictionary<string, string> dic;
 
-		public static HttpClient client;
-		public static Dictionary<string, string> dic;
+        public RestService()
+        {
+        }
 
-		public RestService() {
-		}
+        public static async Task<List<Beer>> Request(Dictionary<string, string> dic, string page)
+        {
 
-		public static async Task<List<Beer>> Request(Dictionary<string, string> dic, string page) {
+            if (client == null) client = new HttpClient();
+            Dictionary<string, string> values = dic;
+            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+            HttpResponseMessage response = await client.PostAsync("http://51.38.239.219/admin/" + page + ".php", content).ConfigureAwait(false);
+            string responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Beer>>(responseString);
+        }
 
-			if(client == null) client = new HttpClient();
-			Dictionary<string, string> values = dic;
-			FormUrlEncodedContent content = new FormUrlEncodedContent(values);
-			HttpResponseMessage response = await client.PostAsync("http://51.38.239.219/admin/" + page + ".php", content).ConfigureAwait(false);
-			string responseString = await response.Content.ReadAsStringAsync();
-			return JsonConvert.DeserializeObject<List<Beer>>(responseString);
-		}
-	}
+        public static async Task<List<User>> RequestUser(Dictionary<string, string> dic, string page)
+        {
+
+            if (client == null) client = new HttpClient();
+            Dictionary<string, string> values = dic;
+            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+            HttpResponseMessage response = await client.PostAsync("http://51.38.239.219/admin/" + page + ".php", content).ConfigureAwait(false);
+            string responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<User>>(responseString);
+        }
+
+        public static async Task<List<Succes>> RequestSucces(Dictionary<string, string> dic, string page)
+        {
+
+            if (client == null) client = new HttpClient();
+            Dictionary<string, string> values = dic;
+            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+            HttpResponseMessage response = await client.PostAsync("http://51.38.239.219/admin/" + page + ".php", content).ConfigureAwait(false);
+            string responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Succes>>(responseString);
+        }
+
+    }
 }
