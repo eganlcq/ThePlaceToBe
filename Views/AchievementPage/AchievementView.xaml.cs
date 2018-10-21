b7ce7594c5a15154aadac4fd7c8f32a17550b2b5
jordanvkb201460 <33064840+jordanvkb201460@users.noreply.github.com>
2018-10-21 17:56:33 +0200

    Add files via upload

--- Views/AchievementPage/AchievementView.xaml.cs
+++ Views/AchievementPage/AchievementView.xaml.cs
@@ -7,14 +7,55 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace ThePlaceToBe.Views.AchievementPage
+namespace ThePlaceToBe
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AchievementView : ContentView
-	{
-		public AchievementView ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AchievementView : ContentView
+    {
+        public AchievementView(string idUser)
+        {
+            InitializeComponent();
+            RestService.dic = RestService.dic = new Dictionary<string, string> {
+
+                {"idUser", idUser}
+            };
+            List<Succes> listSucces = RestService.RequestSucces(RestService.dic, "selectAchievement").Result;
+            int nbSucces = listSucces.Count();
+            double nbRow = Math.Ceiling(nbSucces / 3.0);
+            double nbColumn = 3;
+            int count = 0;
+
+            for (int i = 0; i < nbRow; i++)
+            {
+
+                RowDefinition row = new RowDefinition
+                {
+                    Height = 100
+                };
+                succesGrid.RowDefinitions.Add(row);
+            }
+
+
+            for (int x = 0; x < nbRow; x++)
+            {
+
+                for (int y = 0; y < nbColumn && count < nbSucces; y++, count++)
+                {
+
+                    Succes succes = listSucces[count];
+                    //string imgBiere = listSucces[count].Image;
+                    //Image img;
+                    Label tmpLab = new Label
+                    {
+                        Text = listSucces[count].nom,
+                        TextColor = Color.Black,
+                        FontSize = 11
+                    };
+
+
+                    succesGrid.Children.Add(tmpLab, y, x);
+                }
+            }
+        }
+    }
 }
--- Views/AchievementPage/DonneesView.xaml.cs
+++ Views/AchievementPage/DonneesView.xaml.cs
@@ -7,14 +7,23 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace ThePlaceToBe.Views.AchievementPage
+namespace ThePlaceToBe
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DonneesView : ContentView
 	{
-		public DonneesView ()
-		{
-			InitializeComponent ();
-		}
-	}
+        public DonneesView(string idUser)
+        {
+            InitializeComponent();
+            RestService.dic = RestService.dic = new Dictionary<string, string> {
+
+                {"idUser", idUser}
+            };
+            List<User> listUser = RestService.RequestUser(RestService.dic, "selectUser").Result;
+            User user = listUser[0];
+            nom.Text = user.nom;
+            prenom.Text = user.prenom;
+            email.Text = user.email;
+        }
+    }
 }
