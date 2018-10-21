28ac17437013ec46097937f528a52c4f541b64b9
jordanvkb201460 <33064840+jordanvkb201460@users.noreply.github.com>
2018-10-21 18:42:41 +0200

    Add files via upload

---
+++ Site_Web/admin/selectAchievement.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'database.php';
+    $idUser = "";
+    if(!empty($_POST)) {
+      
+        $idUser = $_POST['idUser'];
+    }
+    $db = Database::connect();
+    $sql =  'SELECT t1.idsucces, t1.nom, t1.descr FROM TbSucces as t1
+JOIN TbOrnement as t2
+ON t1.idsucces = t2.idsucces
+JOIN TbUser as t3
+ON t3.iduser = t2.iduser
+WHERE t3.iduser = ?;';
+    $statement = $db->prepare($sql);
+    $statement->execute([$idUser]);
+    $item = $statement->fetchAll(PDO::FETCH_ASSOC);
+    Database::disconnect();
+    echo json_encode($item);
+?>
