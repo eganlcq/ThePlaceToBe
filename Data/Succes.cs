4d91beed4ac9076eaa055174223af5cc70e70162
jordanvkb201460 <33064840+jordanvkb201460@users.noreply.github.com>
2018-10-21 17:59:34 +0200

    Add files via upload

---
+++ Data/Succes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePlaceToBe
+{
+   
+    class Succes
+    {
+        public int idsucces;
+        public string nom;
+        public string descr;
+    }
+}
