8b7103a09f245313b4ff6dda4c0a6c89d0ecf6a1
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-10-21 17:28:01 +0200

    Add files via upload

--- DB/Procédures/barlocalisation.sql
+++ DB/Procédures/barlocalisation.sql
@@ -3,6 +3,6 @@ BEGIN
 SELECT tbba.idbar, tbba.nombar, tbba.latitude, tbba.longitude
 FROM TbBar AS tbba
 JOIN TbCarte AS tbc ON tbc.idbar = tbba.idbar
-JOIN tbBiere AS tbbi ON tbbi.idbiere = tbc.idbiere
+JOIN TbBiere AS tbbi ON tbbi.idbiere = tbc.idbiere
 WHERE tbbi.idbiere = beerid;
 END
