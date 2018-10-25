5165b55bb82473d8c9f5449cf80ad41ddef90cc0
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-10-25 20:46:38 +0200

    Update userinscriptionapp.sql

--- DB/Procédures/userinscriptionapp.sql
+++ DB/Procédures/userinscriptionapp.sql
@@ -1,5 +1,5 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `userinscriptionapp`(nom varchar(32), prenom varchar(32), pseudo varchar(32), mail varchar(64), datenaiss date, mdp varchar(64))
 BEGIN
 INSERT INTO TbUser (prenom, nom, pseudo, datenaiss, email, mdp)
-VALUES(prenom, nom, pseudo, mail, CAST(datenaiss AS date), mdp);
+VALUES(prenom, nom, pseudo, CAST(datenaiss AS date), mail,  mdp);
 END
