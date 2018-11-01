ecc9d197a9734040f1192e64c4997fd241069f91
WyartGuillaume <32869680+Maestro700@users.noreply.github.com>
2018-11-01 19:51:29 +0100

    Add files via upload

--- Site_Web/index.php
+++ Site_Web/index.php
@@ -7,10 +7,11 @@ if(isset($_GET['rq']) && !empty($_GET['rq'])){
     gereRequest($_GET['rq']);
     die(json_encode($tosend));
 }
+
 $compte = "<h1><a href='login.html'>Mon compte</a></h1>";
 
 if(isAuthenticated()){
-    $compte = "<img id=avatar src=".$_SESSION['user']['photo']." height=50px width=50px><p>".$_SESSION['user']['pseudo']."</p><p id='liste'>&#9662;</p>";
+    $compte = "<img id=avatar src=image/avatar/".$_SESSION['user']['photo']." height=50px width=50px><p>".$_SESSION['user']['pseudo']."</p><p id='liste'>&#9662;</p>";
 }
 
 require_once 'INC/mainPage.inc.php';
