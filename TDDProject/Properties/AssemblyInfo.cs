bc2b30c782dcfc9fd2dd0dbd20a35043f2deda7c
Egan Lecocq <e.lecocq@hotmail.com>
2018-11-09 22:42:40 +0100

    Test Inscription

---
+++ TDDProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDDProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c019dd6-f2ff-481c-9778-6282d5b0b2f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThePlaceToBe/ThePlaceToBe/Data/Process.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePlaceToBe.Data
+{
+    public static class Process
+    {
+		// DEBUT METHODES INSCRIPTION
+
+		public static string VerifyInscription(string firstNameUser, string nameUser, string pseudoUser, string emailUser, string pswdUser, string confirmPswdUser, string birthDate) {
+
+			// Vérification si tout les champs sont remplis
+			if (CheckInfo(null, firstNameUser, nameUser, pseudoUser, emailUser, pswdUser, confirmPswdUser, birthDate)
+				&& 
+				CheckInfo("", firstNameUser, nameUser, pseudoUser, emailUser, pswdUser, confirmPswdUser, birthDate)) {
+
+				// Vérification si l'utilisateur a rentré deux fois le même mot de passe
+				if (CheckSamePswd(pswdUser, confirmPswdUser)) {
+
+					// Vérifie si le pseudo existe déjà dans la base de donnée
+					if (CheckPseudo(pseudoUser)) {
+
+						// Vérifie si le mail existe déjà dans la base de donnée
+						if (CheckMail(emailUser)) {
+
+							// Effectue l'inscription de l'utilisateur
+							return "OK";
+						}
+						else {
+
+							return "Le mail existe déjà.";
+						}
+					}
+					else {
+
+						return "Le pseudo existe déjà.";
+					}
+				}
+				else {
+
+					return "Les mots de passe ne correspondent pas.";
+				}
+			}
+			else {
+
+				return "Veuillez remplir tous les champs s'il vous plaît";
+			}
+		}
+
+		public static bool CheckInfo(string obj, string firstNameUser, string nameUser, string pseudoUser, string emailUser, string pswdUser, string confirmPswdUser, string birthDate) {
+
+			if (firstNameUser != obj && nameUser != obj && pseudoUser != obj && emailUser != obj && pswdUser != obj && confirmPswdUser != obj && birthDate != obj) {
+
+				return true;
+			}
+			else return false;
+		}
+
+		public static bool CheckSamePswd(string pswdUser, string confirmPswdUser) {
+
+			if (pswdUser == confirmPswdUser) return true;
+			else return false;
+		}
+
+		public static bool CheckPseudo(string pseudoUser) {
+
+			RestService.dic = new Dictionary<string, string> {
+
+				{ "pseudo", pseudoUser }
+			};
+			Check check = RestService.Request<Check>(RestService.dic, "checkPseudo").Result[0];
+			return check.Verif;
+		}
+
+		public static bool CheckMail(string emailUser) {
+
+			RestService.dic = new Dictionary<string, string> {
+
+				{ "email", emailUser }
+			};
+			Check check = RestService.Request<Check>(RestService.dic, "checkMail").Result[0];
+			return check.Verif;
+		}
+
+		// FIN METHODES INSCRIPTION
+	}
+}
