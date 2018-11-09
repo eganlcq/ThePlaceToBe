36fdaf898ca706101326c3c7646cbe67f74ba91c
Egan Lecocq <e.lecocq@hotmail.com>
2018-11-09 20:05:17 +0100

    add application

---
+++ ThePlaceToBe/ThePlaceToBe.Android/obj/Debug/MonoAndroid81/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("ThePlaceToBe.Data.MainApplication, ThePlaceToBe.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md51bee8068697403abbae45f654ae0b682.MainApplication.class, md51bee8068697403abbae45f654ae0b682.MainApplication.__md_methods);
+		
+	}
+}
