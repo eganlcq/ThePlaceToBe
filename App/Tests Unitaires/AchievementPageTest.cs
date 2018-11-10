af4ca330649644626859567b8a956399c7b613ba
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-11-10 13:11:02 +0100

    Add files via upload

---
+++ App/Tests Unitaires/AchievementPageTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThePlaceToBe.Data;
+
+namespace TDDProject
+{
+    [TestClass]
+    public class AchievementPageTest
+    {
+        [TestMethod]
+        public void RecieveUser_WithAnId_returnUserNotNUll()
+        {
+            // Arrange
+            string idUser = "1";
+
+            // Act
+            List<User> listWithOneUser = Process.GetUser(idUser);
+
+            // Assert
+            Assert.IsNotNull(listWithOneUser);
+        }
+
+        [TestMethod]
+        public void RecieveUser_WithAnId_returnPseudoOfUser()
+        {
+
+            // Arrange
+            string idUser = "1";
+
+            // Act
+            List<User> listWithOneUser = Process.GetUser(idUser);
+
+            // Assert
+            Assert.AreEqual("JEJ", listWithOneUser[0].Pseudo);
+        }
+    }
+}
