d85d070bab4c7c3467f8b173612e86d95ce93f48
eganlcq <32932521+eganlcq@users.noreply.github.com>
2018-11-10 12:50:11 +0100

    Add files via upload

---
+++ App/Tests Unitaires/RestServiceTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThePlaceToBe.Data;
+
+namespace TDDProject {
+	[TestClass]
+	public class RestServiceTest {
+
+		[TestMethod]
+		public void IsConnexionAvailable_WithGoodPseudoAndPassword_ReturnsCurrentUser() {
+
+			// Arrange
+			var dic = new Dictionary<string, string> {
+				{ "pseudo", "Octofen" },
+				{ "pswd", "jej"}
+			};
+
+			// Act
+			User currentUser = RestService.Request<User>(dic, "userConnexion").Result[0];
+
+			// Assert
+			Assert.AreEqual("Octofen", currentUser.Pseudo);
+			Assert.AreEqual("Egan", currentUser.Prenom);
+			Assert.AreEqual("Lecocq", currentUser.Nom);
+		}
+
+		[TestMethod]
+		public void CanWeGetTheListOfBeers_ReturnsTheListOfBeers() {
+
+			// Arrange
+			var dic = new Dictionary<string, string>();
+			
+			// Act
+			List<Beer> listBiere = RestService.Request<Beer>(dic, "selectBeer").Result;
+
+			// Assert
+			Assert.AreNotEqual(0, listBiere.Count);
+		}
+
+		[TestMethod]
+		public void CanWeGetOneBeer_WithIdOfTheBeer_ReturnsOneBeer() {
+
+			// Arrange
+			var dic = new Dictionary<string, string> {
+
+				{"idBiere", "1" }
+			};
+
+			// Act
+			Beer beer = RestService.Request<Beer>(dic, "selectOneBeer").Result[0];
+
+			// Assert
+			Assert.AreEqual("Ambrasse-Temps", beer.Nombiere);
+		}
+	}
+}
