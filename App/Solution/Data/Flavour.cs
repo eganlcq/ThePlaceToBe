f7a678ca6325dfb3747ea91b59b23a3041225a19
eganlcq <32932521+eganlcq@users.noreply.github.com>
2018-11-17 15:02:38 +0100

    Add files via upload

---
+++ App/Solution/Data/Flavour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePlaceToBe.Data
+{
+    class Flavour
+    {
+		public string Typebiere { get; set; }
+
+		public Flavour() {
+
+		}
+    }
+}
