e8f2107f1e60fd0a463719fe8a7d3034cfef5339
eganlcq <32932521+eganlcq@users.noreply.github.com>
2018-11-18 15:34:45 +0100

    Add files via upload

--- App/Solution/Views/ProductPage/ProductPage.xaml.cs
+++ App/Solution/Views/ProductPage/ProductPage.xaml.cs
@@ -10,118 +10,122 @@ using Xamarin.Forms.Xaml;
 
 namespace ThePlaceToBe.Views.ProductPage
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ProductPage : ContentPage
-    {
-        public ProductPage()
-        {
-            InitializeComponent();
-            NavigationPage.SetHasNavigationBar(this, false);
-            // Initialise des éléments présents dans le xaml
-            Init();
-            // Initialise des éléments pour faire fonctionner la map
-            InitMap();
-        }
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProductPage : ContentPage {
+		public ProductPage(Beer beer) {
+			InitializeComponent();
+			NavigationPage.SetHasNavigationBar(this, false);
+			// Initialise des éléments présents dans le xaml
+			Init(beer);
+			// Initialise des éléments pour faire fonctionner la map
+			InitMap();
+		}
 
-        // Méthode lancée lorsque l'on clique sur l'image du user
-        private void ProfilMainPageTapped(object sender, EventArgs e)
-        {
-            this.Navigation.PushAsync(new AchievementPage.AchievementPage(User.currentUser.Iduser.ToString()));
-        }
+		// Méthode lancée lorsque l'on clique sur l'image du user
+		private void ProfilMainPageTapped(object sender, EventArgs e) {
+			this.Navigation.PushAsync(new AchievementPage.AchievementPage(User.currentUser.Iduser.ToString()));
+		}
 
-        // Méthode lancée lorsque le bouton de retour est utilisé
-        private void BtnRetourClicked(object sender, EventArgs e)
-        {
-            this.Navigation.PopAsync();
-        }
+		// Méthode lancée lorsque le bouton de retour est utilisé
+		private void BtnRetourClicked(object sender, EventArgs e) {
+			this.Navigation.PopAsync();
+		}
 
-        // Méthode lancée lorsque le bouton d'ajout de favoris est utilisé
-        private void BtnAjoutFavorisClicked(object sender, EventArgs e, Beer beer)
-        {
-            RestService.dic = new Dictionary<string, string> {
+		// Méthode lancée lorsque le bouton d'ajout de favoris est utilisé
+		private void AddFav(object sender, EventArgs e, Beer beer) {
+			RestService.dic = new Dictionary<string, string> {
 
-                { "idBeer", beer.Idbiere.ToString()},
-                { "idUser", User.currentUser.Iduser.ToString()}
-            };
-            List<Beer> listBiere = RestService.Request<Beer>(RestService.dic, "insertFavoris").Result;
-        }
+				{ "idBeer", beer.Idbiere.ToString()},
+				{ "idUser", User.currentUser.Iduser.ToString()}
+			};
+			RestService.Request(RestService.dic, "insertFavoris");
+		}
 
-        // Affiche les info de la bière courante sur la page
-        private void DisplayBeerInfo(Beer beer)
-        {
+		private void RemoveFav(object sender, EventArgs e, Beer beer) {
 
-            lblName.Text = beer.Nombiere;
-            lblAlcool.Text = beer.Alcoolemie.ToString() + '%';
-            lblSaveur.Text = beer.Typebiere;
-            if (beer.Image != "" && beer.Image != null)
-            {
+			RestService.dic = new Dictionary<string, string> {
 
-                imgBeer.Source = Constants.beersImg + beer.Image;
-            }
-            else
-            {
+				{ "idBeer", beer.Idbiere.ToString()},
+				{ "idUser", User.currentUser.Iduser.ToString()}
+			};
+			RestService.Request(RestService.dic, "deleteFavoris");
+		}
 
-                imgBeer.Source = Constants.beersImg + "oneBeer.png";
-            }
-        }
+		// Affiche les info de la bière courante sur la page
+		private void DisplayBeerInfo(Beer beer) {
 
-        // Initialise des éléments présents dans le xaml
-        private void Init()
-        {
+			lblName.Text = beer.Nombiere;
+			lblAlcool.Text = beer.Alcoolemie.ToString() + '%';
+			lblSaveur.Text = beer.Typebiere;
+			if (beer.Image != "" && beer.Image != null) {
 
-            imgLogo.Source = Constants.appImg + "logo.png";
-            imgAccount.Source = Constants.userImg + User.currentUser.Photo;
-            lblPseudo.Text = User.currentUser.Pseudo;
-            Beer beer = RestService.Request<Beer>(RestService.dic, "selectOneBeer").Result[0];
-            DisplayBeerInfo(beer);
-            btnFavoris.Clicked += (s, e) => BtnAjoutFavorisClicked(s, e, beer);
-        }
+				imgBeer.Source = Constants.beersImg + beer.Image;
+			}
+			else {
 
-        // Initialise des éléments pour faire fonctionner la map
-        private void InitMap()
-        {
+				imgBeer.Source = Constants.beersImg + "oneBeer.png";
+			}
+		}
 
-            List<Bar> listBar = RestService.Request<Bar>(RestService.dic, "selectBar").Result;
-            double lat = 50.669204;
-            double lon = 4.613774;
-            Position pos = new Position(lat, lon);
+		// Initialise des éléments présents dans le xaml
+		private void Init(Beer beer) {
 
-            List<Pin> listPin = AddPinsNeeded(listBar);
-            DisplayMap(pos, listPin);
-        }
+			imgLogo.Source = Constants.appImg + "logo.png";
+			imgAccount.Source = Constants.userImg + User.currentUser.Photo;
+			lblPseudo.Text = User.currentUser.Pseudo;
+			//Beer beer = RestService.Request<Beer>(RestService.dic, "selectOneBeer").Result[0];
+			DisplayBeerInfo(beer);
 
-        // Ajoute un nombre définis de pins dans la liste en fonction du nombre de bar reçu
-        private List<Pin> AddPinsNeeded(List<Bar> listBar)
-        {
+			if(Process.CheckFavorite(beer.Idbiere, User.currentUser.Iduser)) {
 
-            List<Pin> listPin = new List<Pin>();
+				btnFavoris.Text = "Remove Fav";
+				btnFavoris.Clicked += (s, e) => RemoveFav(s, e, beer);
+			}
+			else {
+				btnFavoris.Text = "Add Fav";
+				btnFavoris.Clicked += (s, e) => AddFav(s, e, beer);
+			}
+		}
 
-            foreach (Bar bar in listBar)
-            {
+		// Initialise des éléments pour faire fonctionner la map
+		private void InitMap() {
 
-                Pin pin = new Pin
-                {
-                    Type = PinType.Generic,
-                    Position = new Position(bar.Latitude, bar.Longitude),
-                    Label = bar.Nombar,
-                    Address = ""
-                };
-                listPin.Add(pin);
-            }
-            return listPin;
-        }
+			List<Bar> listBar = RestService.Request<Bar>(RestService.dic, "selectBar").Result;
+			double lat = 50.669204;
+			double lon = 4.613774;
+			Position pos = new Position(lat, lon);
 
-        // Ajoute des pins à la map et change la position de la map
-        private void DisplayMap(Position pos, List<Pin> listPin)
-        {
+			List<Pin> listPin = AddPinsNeeded(listBar);
+			DisplayMap(pos, listPin);
+		}
 
-            map.MoveToRegion(MapSpan.FromCenterAndRadius(pos, Distance.FromKilometers(0.15)));
-            map.MapType = MapType.Street;
-            foreach (Pin pin in listPin)
-            {
+		// Ajoute un nombre définis de pins dans la liste en fonction du nombre de bar reçu
+		private List<Pin> AddPinsNeeded(List<Bar> listBar) {
 
-                map.Pins.Add(pin);
-            }
-        }
-    }
+			List<Pin> listPin = new List<Pin>();
+
+			foreach (Bar bar in listBar) {
+
+				Pin pin = new Pin {
+					Type = PinType.Generic,
+					Position = new Position(bar.Latitude, bar.Longitude),
+					Label = bar.Nombar,
+					Address = ""
+				};
+				listPin.Add(pin);
+			}
+			return listPin;
+		}
+
+		// Ajoute des pins à la map et change la position de la map
+		private void DisplayMap(Position pos, List<Pin> listPin) {
+
+			map.MoveToRegion(MapSpan.FromCenterAndRadius(pos, Distance.FromKilometers(0.15)));
+			map.MapType = MapType.Street;
+			foreach (Pin pin in listPin) {
+
+				map.Pins.Add(pin);
+			}
+		}
+	}
 }
