5b63716d74e40a39e9ae3ed777504e1bb5768c47
WyartGuillaume <32869680+Maestro700@users.noreply.github.com>
2018-11-21 21:50:14 +0100

    Add files via upload

--- Site Web/JS/index.js
+++ Site Web/JS/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#societe a').click(function (event) {
+    $('footer a').click(function (event) {
         event.preventDefault();
         appelAjax(this);
     });
@@ -10,24 +10,28 @@ $(document).ready(function(){
     $('#gestion a').click(function (event) {
         event.preventDefault();
         appelAjax(this);
+        $('#gestion').css('display', 'none');
     });
-    $('#liste').click(function (event) {
+    $('#compte p').click(function (event) {
         event.preventDefault();
-        var bool = ($('#gestion').is(':visible'))? $('#gestion').css('display', bool).fadeOut(500) : $('#gestion').css('display', bool).fadeIn(500);
+        $('#gestion').slideToggle();
     });
 });
 
-function appelAjax(element){
+function appelAjax(element, name=''){
     $.ajaxSetup({ processData: false, contentType : false});
     var data= new FormData();
     var request = 'unknownUri';
     switch(true){
         case Boolean(element.href) : request = $(element).attr('href').split('.html')[0];
             break;
-        case Boolean(element.action) : request = $(element).attr('action').split('.html')[0]; break;
+        case Boolean(element.action) :
+            request = $(element).attr('action').split('.html')[0];
+            break;
     }
     data= new FormData(element);
     data.append('senderId', element.id);
+    data.append('senderName', name);
     $.post('index.php?rq='+request, data, gereRetour);
 }
 
@@ -36,40 +40,56 @@ function gereRetour(retour){
     retour =testeJson(retour);
     for(cle in retour) {
         switch (cle) {
-            case 'about' : $('main').html(retour[cle]);
-                           $("html,body").animate({ scrollTop: 0 }, "slow");
-                           $('#propos div:not(#propos div:first)').hide();
-                           $('#propos li').click(function () {
-                               $('#propos div').hide();
-                               $('#propos li').removeClass('selected');
-                               $(this).addClass('selected');
-                               var divId = parseInt($(this).attr('id'));
-                               $('#propos div:eq('+divId+')').show(500);
-                           });
-                           break;
-            case 'login' : $('body').append(retour[cle]);
-                           $('#inscription').submit(function (event){
-                               event.preventDefault();
-                               appelAjax(this);
-                               $('.log').remove();
-                           });
-                           $('#connexion').submit(function (event){
-                               event.preventDefault();
-                               appelAjax(this);
-                               $('.log').remove();
-                           });
-                           $('#retour').click(function (event) {
-                               event.preventDefault();
-                               $('.log').remove();
-                           });
-                           break;
+            case 'about' :
+                $('main').html(retour[cle]);
+                $("html,body").animate({ scrollTop: 0 }, "slow");
+                $('#propos div:not(#propos div:first)').hide();
+                $('#propos li').click(function () {
+                    $('#propos div').hide();
+                    $('#propos li').removeClass('selected');
+                    $(this).addClass('selected');
+                    var divId = parseInt($(this).attr('id'));
+                    $('#propos div:eq('+divId+')').show(500);
+                });
+                break;
+            case 'login' :
+                $('body').append(retour[cle]);
+
+                if($('body').outerWidth(true) < 500) {
+                    $("<p id='CreateCompte'>Créer un compte</p>").insertAfter("#connexion input[type=submit]")
+                    $('#inscription').hide();
+                    $(document).on('click', '#CreateCompte', function (event) {
+                        event.preventDefault();
+                        $(this).remove();
+                        $('#inscription').show();
+                        $('#connexion').hide();
+                    });
+                }
+
+                $('#inscription').submit(function (event){
+                    event.preventDefault();
+                    appelAjax(this);
+                    $('.log').remove();
+                });
+
+                $('#connexion').submit(function (event){
+                    event.preventDefault();
+                    appelAjax(this);
+                    $('.log').remove();
+                });
+
+                $('#retour').click(function (event) {
+                    event.preventDefault();
+                    $('.log').remove();
+                });
+                break;
             case 'connexion':
                 var x= JSON.parse(retour[cle]);
-                var affiche = "<img id=avatar src=image/avatar/"+x['photo']+" height=50px width=50px>"+"<p>"+x["pseudo"]+"</p><p id='liste'>&#9662;</p>";
+                var affiche = "<img id=avatar src=image/avatar/"+x['photo']+" height=50px width=50px alt='Avatar'>"+"<p>"+x["pseudo"]+"</p><p id='liste'>&#9662;</p>";
                 $('#compte').html(affiche);
-                $('#liste').click(function (event) {
+                $('#compte p').click(function (event) {
                     event.preventDefault();
-                    var bool = ($('#gestion').is(':visible'))? $('#gestion').css('display', bool).fadeOut(500) : $('#gestion').css('display', bool).fadeIn(500);
+                    $('#gestion').slideToggle();
                 });
                 break;
             case 'logout':
@@ -98,6 +118,12 @@ function gereRetour(retour){
                     event.preventDefault();
                     appelAjax(this);
                 });
+                $('#gestionCompte a').click(function (event) {
+                    event.preventDefault();
+                    $('#listeGestionCompte a').removeClass('selected');
+                    $(this).addClass('selected');
+                    appelAjax(this);
+                });
                 break;
             case "erreur1":
                 $('#erreur1').html(retour[cle]);
@@ -108,9 +134,35 @@ function gereRetour(retour){
             case "erreur3":
                 $('#erreur3').html(retour[cle]);
                 break;
+            case 'erreur4':
+                $('#erreur4').html(retour[cle]);
+                break;
             case 'achiev':
-                $('main').html(retour[cle]);
+                $('#contenuCompte').html(retour[cle]);
                 $('main a').click(function (event) {
+                    event.preventDefault();
+                    appelAjax(this);
+                });
+                break;
+            case 'gestionCompte':
+                $('#contenuCompte').html(retour[cle]);
+                $('#table form input[type=submit]').click(function (e) {
+                    var x = e.target.name;
+                    $('#table form').submit(function (event) {
+                        event.preventDefault();
+                        appelAjax(this, x);
+                    });
+                });
+                $('#contenuCompte a').click(function (event) {
+                    event.preventDefault();
+                    appelAjax(this);
+                });
+                break;
+            case 'adminconnexion':
+                $('#contenuCompte').html(retour[cle]);
+                $('#listeGestionCompte a').removeClass('selected');
+                $('#listeGestionCompte a:first').addClass('selected');
+                $("#formAdmin").submit(function (event) {
                     event.preventDefault();
                     appelAjax(this);
                 });
