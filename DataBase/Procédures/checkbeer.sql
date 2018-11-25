81f7c4f1db6b827cd74ec7194c0dbf57c396d9af
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-11-25 16:18:58 +0100

    Add files via upload

--- DataBase/Procédures/checkbeer.sql
+++ DataBase/Procédures/checkbeer.sql
@@ -1,5 +1,5 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `checkbeer`(beer varchar(32))
 BEGIN
-SELECT DISTINCT IF(beer IN (SELECT tbbe.nombiere FROM TbBiere AS tbbe), 'FALSE', 'TRUE') AS verif
+SELECT DISTINCT IF(beer IN (SELECT tbbe.nombiere FROM TbBiere AS tbbe), 'TRUE', 'FALSE') AS verif
 FROM TbBiere;
 END
--- DataBase/Procédures/checkbeerinbar.sql
+++ DataBase/Procédures/checkbeerinbar.sql
@@ -2,6 +2,6 @@ CREATE DEFINER=`admin`@`%` PROCEDURE `checkbeerinbar`(beer varchar(32), bar varc
 BEGIN
 SELECT DISTINCT IF(
 	(SELECT idbiere FROM TbBiere WHERE nombiere = beer) in (SELECT DISTINCT idbiere FROM TbCarte WHERE idbar = (SELECT idbar FROM TbBar WHERE nombar = bar))
-    ,'FALSE', 'TRUE') as verif
+    ,'TRUE', 'FALSE') as verif
 FROM TbCarte;
 END
