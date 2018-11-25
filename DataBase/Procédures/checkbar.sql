d44e13f45015fdf5076977471b11e47cb82de946
LucaMellini <32932623+LucaMellini@users.noreply.github.com>
2018-11-25 16:16:14 +0100

    Add files via upload

--- DataBase/Procédures/checkbar.sql
+++ DataBase/Procédures/checkbar.sql
@@ -1,5 +1,5 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `checkbar`(bar varchar(32))
 BEGIN
-SELECT DISTINCT IF(bar IN (SELECT tbba.nombar FROM TbBar AS tbba), 'FALSE', 'TRUE') AS verif
+SELECT DISTINCT IF(bar IN (SELECT tbba.nombar FROM TbBar AS tbba), 'TRUE', 'FALSE') AS verif
 FROM TbBar;
 END
