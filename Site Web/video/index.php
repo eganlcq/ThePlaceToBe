fe49deda7de54d72512d018c52f4da8453953b5f
JacobsDavid <37386477+JacobsDavid@users.noreply.github.com>
2018-11-26 23:31:24 +0100

    Add files via upload

---
+++ Site Web/video/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Retour à la case départ</title>
+        <meta http-equiv="refresh" content="2; URL=..">
+    </head>
+    <body>
+        You are not allowed to access that directory
+    </body>
+</html>
