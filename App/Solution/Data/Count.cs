cb8eeec3ca5fe90d142c9690bbc43f3935af79d4
jordanvkb201460 <33064840+jordanvkb201460@users.noreply.github.com>
2018-11-26 16:55:38 +0100

    Add files via upload

---
+++ App/Solution/Data/Count.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    //MODIFIER LE NAMESPACE !!!!!!
+    class Count
+    {
+        public int Counter { get; set; }
+    }
+}
