28c82b9d619ed7463bdbf5216084e96c9d593d00
WyartGuillaume <32869680+Maestro700@users.noreply.github.com>
2018-12-17 23:15:27 +0100

    Add files via upload

---
+++ Site Web/JS/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Retour à la case départ</title>
+        <meta http-equiv="refresh" content="2; URL=..">
+    </head>
+    <body>
+        Vous n'êtes pas autorisé à accéder à ce dossier
+    </body>
+</html>
