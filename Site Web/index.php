1a27c28761d711c9781f4070c38b7a75d0b3d3fb
WyartGuillaume <32869680+Maestro700@users.noreply.github.com>
2018-12-17 23:24:55 +0100

    Add files via upload

--- Site Web/index.php
+++ Site Web/index.php
@@ -1,17 +1,11 @@
-<?php
-require_once 'INC/request.inc.php';
-
-session_start();
-
-if(isset($_GET['rq']) && !empty($_GET['rq'])){
-    gereRequest($_GET['rq']);
-    die(json_encode($tosend));
-}
-
-$compte = "<h1><a href='login.html'>Mon compte</a></h1>";
-
-if(isAuthenticated()){
-    $compte = "<img id=avatar src=image/avatar/".$_SESSION['user']['photo']." height=50px width=50px alt='Logo_Compte'><p>".$_SESSION['user']['pseudo']."</p><p id='liste'>&#9662;</p>";
-}
-
-require_once 'INC/mainPage.inc.php';
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Retour à la case départ</title>
+        <meta http-equiv="refresh" content="2; URL=..">
+    </head>
+    <body>
+        You are not allowed to access that directory
+    </body>
+</html>
